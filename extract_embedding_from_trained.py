113337ffc244a223861f2427a8cc2cf974535d70
raminass <raminass@gmail.com>
2022-10-13 09:53:04 +0300

    update requ and add extractor

---
+++ extract_embedding_from_trained.py
@@ -0,0 +1,26 @@
+from modeling.models import TransformerModel
+import torch
+import pandas as pd
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+name = "Oct06_19-46-44_s-001"
+checkpoint = torch.load(f"artifacts/{name}_model.pt")
+model = TransformerModel(checkpoint["model_params"], checkpoint["networks"]).to(device)
+model.load_state_dict(checkpoint["model_state_dict"])
+
+model.eval()
+for net in checkpoint["networks"]:
+    net["node_tokens"] = torch.tensor(checkpoint["tokenizer"].encode(net["node_sequence"][0]).ids).to(device)
+    net.edge_index = net.edge_index.to(device)
+    net.edge_weight = net.edge_weight.to(device)
+
+model.all_prop_emb()
+
+all_emb = []
+for net in checkpoint["networks"]:
+    x = model.encoder(net.node_tokens)
+    all_emb.append(pd.DataFrame(x.detach().cpu().numpy(), index=net.node_index))
+emb_df = pd.concat(all_emb).drop_duplicates()
+
+emb_df.to_csv(f"""outputs/{name}_{checkpoint["model_params"]["K"]}_feat.tsv""", sep="\t")
--- utils/commons.py
+++ utils/commons.py
@@ -31,9 +31,6 @@ def read_data(graphs, union, model_params):
         pyg_graph["node_sequence"] = []
         pyg_graph["node_sequence"].append(" ".join(node for node in pyg_graph.node_index))
         netwroks.append(pyg_graph)
-    # data = ['YHR147C YJL023C YPL040C YLR091W YPL189C-A YIL157C',
-    #         'YBR250W YDL086W YLR159C-A YDR519W YJL052W YBR199W',
-    #         'YNL309W YGR161C YDR542W YML109W YJL221C YLR151C']
 
     # reading union network
     g = nx.read_weighted_edgelist(union, delimiter=" ").to_undirected()
@@ -49,55 +46,3 @@ def read_data(graphs, union, model_params):
     pyg_un["node_sequence"].append(" ".join(node for node in pyg_un.node_index))
     netwroks.append(pyg_un)
     return data, netwroks, pyg_un
-
-
-# def my_tokenizer(data):
-#     tokenizer = Tokenizer(models.WordLevel(unk_token="[UNK]"))
-#     my_tokens = Path("artifacts/tokenizer.pt")
-#     if my_tokens.exists():
-#         print("Reading Tokens from Disk")
-#         tokenizer = tokenizer.from_file(my_tokens._str)
-#     else:
-#         print("training tokenizer!")
-#         tokenizer.pre_tokenizer = WhitespaceSplit()
-#         trainer = trainers.WordLevelTrainer(special_tokens=["[UNK]", "[MASK]", "[CLS]", "[PAD]"])
-#         tokenizer.enable_padding()
-#         tokenizer.train_from_iterator(map(lambda x: x.split(), data), trainer=trainer)
-#         tokenizer.save(my_tokens._str)
-#     return tokenizer
-
-
-# tokenizer.get_vocab()
-# output = tokenizer.encode('YHR147C YJL023C YPL040C')
-# output.ids
-
-
-# class MyDataset(Dataset):
-#     def __init__(self, src, tokenizer):
-#         self.src = [i for i in src]
-#         self.tokenizer = tokenizer
-
-#     def __len__(self):
-#         return len(self.src)
-
-#     def __getitem__(self, idx):
-#         src = self.src[idx]
-#         return src
-
-
-# class PositionalEncoding(nn.Module):
-#     def __init__(self, d_model, dropout=0.1, max_len=512):
-#         super(PositionalEncoding, self).__init__()
-#         self.dropout = nn.Dropout(p=dropout)
-
-#         pe = torch.zeros(max_len, d_model)
-#         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-#         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-#         pe[:, 0::2] = torch.sin(position * div_term)
-#         pe[:, 1::2] = torch.cos(position * div_term)
-#         pe = pe.unsqueeze(0).transpose(0, 1)
-#         self.register_buffer("pe", pe)
-
-#     def forward(self, x):
-#         x = x + self.pe[: x.size(0), :]
-#         return self.dropout(x)
